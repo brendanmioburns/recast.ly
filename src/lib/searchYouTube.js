8f1b04a211e272c4ce3887d122a341b58a3b878a
Hack Reactor Student <student@hackreactor.com>
2017-06-03 13:12:49 -0700

    Passed Test: Get, key, query, max options send them in Get request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,21 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  options.query = options.query || 'cats';
+  options.max = options.max || 10;
+  options.key = options.key || window.YOUTUBE_API_KEY;
+  
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + options.query + '&type=video&key=' + options.key + '&maxResults=' + options.max + '&videoEmbeddable=true',
+    type: 'GET',
+    contentType: 'application/json',
+    
+    success: function(data) {
+      console.log(data.items);
+      callback(data.items);
+    },
+    error: function(data) {
+      console.error('failed to get video', data.items);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
